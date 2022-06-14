12d7a874c854dc176108923da1279cf8c5941430
sweethuman <gheo@sweethuman.tech>
2022-06-15 00:41:49 +0300

    feat: change dynaconf env var prefix

--- BlockChain/Application.py
+++ BlockChain/Application.py
@@ -14,9 +14,6 @@ class UI:
         print(figlet_format("Singularity Network", font="slant"))
 
         keyfile = None
-        # argv[0] - name of program
-        # argv[1] - ip
-        # argv[0] - port
         ip = sys.argv[1]
         port = int(sys.argv[2])
         api_port = int(sys.argv[3])
@@ -28,27 +25,6 @@ class UI:
         node = Node(ip, port, keyfile)
         node.startP2P(origin_ip, origin_port)
         node.startAPI(ip, api_port)
-
-        # used for testing on multiple ports
-        # if port == 10002:
-        #     node.p2p.connect_with_node("localhost", 10001)
-
-        # json_object = json.dumps(package, indent = 4)
-        # # Writing to sample.json
-        # with open("test_req.json", "w") as outfile:
-        #     outfile.write(json_object)
-        # request = requests.posst(url, json=package)
-        # print(request.text)
-
-        # print(node.blockchain.toJson())
-        # # print(node.wallet.toJson())
-
-        # print("💰 wallets created ! 💰")
-
-        # print("💳 Transactions are pending ! 💳")
-
-        # # Processing pending transactions
-        # print("⛏️ Node working ... ⛏️")
 
 
 ui = UI()
