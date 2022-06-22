0fab5f3b85ddea792de88c03e4b39e507108423c
sweethuman <gheo@sweethuman.tech>
2022-06-22 19:58:00 +0300

    refactor: configure via dynaconf ip and port settings

--- BlockChain/Application.py
+++ BlockChain/Application.py
@@ -4,6 +4,7 @@ from pyfiglet import figlet_format
 
 from BlockChain.Node import Node
 from BlockChain.NodeAPI import NodeAPI
+from config import settings
 
 
 class UI:
@@ -15,11 +16,11 @@ class UI:
         print(figlet_format("Singularity Network", font="slant"))
 
         keyfile = None
-        ip = sys.argv[1]
-        port = int(sys.argv[2])
-        api_port = int(sys.argv[3])
-        origin_ip = sys.argv[4]
-        origin_port = int(sys.argv[5])
+        ip = sys.argv[1] if len(sys.argv) > 1 else settings.host_ip
+        port = int(sys.argv[2]) if len(sys.argv) > 2 else settings.host_port
+        api_port = int(sys.argv[3]) if len(sys.argv) > 3 else settings.host_api_port
+        origin_ip = sys.argv[4] if len(sys.argv) > 4 else settings.origin_ip
+        origin_port = int(sys.argv[5]) if len(sys.argv) > 5 else settings.origin_port
         if len(sys.argv) > 7:
             keyfile = sys.argv[6], sys.argv[7]
 
